043dfe399b23cb31859e327d465eb2434de3d79c
hedong <13635388534@163.com>
2019-11-14 11:31:23 +0800

    spring cloud服务注册于发现

---
+++ hdspringcloud/eurekaclient/src/main/java/com/springcloud/eurekaclient/EurekaclientApplication.java
@@ -0,0 +1,28 @@
+package com.springcloud.eurekaclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableEurekaClient  //这个注解表明自己是一个服务提供者
+@SpringBootApplication
+public class EurekaclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaclientApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam String name) {
+        return "hi" + name + ",i am from port :" + port;
+    }
+
+}
---
+++ hdspringcloud/eurekaserver/src/main/java/com/srpingcloud/eurekaserver/EurekaserverApplication.java
@@ -0,0 +1,16 @@
+package com.srpingcloud.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+// 服务注册中心
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaserverApplication.class, args);
+    }
+
+}
